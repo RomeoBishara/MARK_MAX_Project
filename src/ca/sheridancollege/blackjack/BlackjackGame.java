f7fde4d79500ee084347610d6f355118bf8223e1
Romeo <HpR@Hp1>
2020-08-16 17:09:04 -0400

    Complete the game class

--- src/ca/sheridancollege/blackjack/BlackjackGame.java
+++ src/ca/sheridancollege/blackjack/BlackjackGame.java
@@ -2,6 +2,8 @@
 
 package ca.sheridancollege.blackjack;
 
+import java.util.Scanner;
+
 
 
 /**
@@ -13,8 +15,12 @@ public class BlackjackGame extends Game{
     
   private BlackJackDealer dealer;
   private BlackJackPlayer player;
+  private final int[] bet = {5, 10, 15, 20};
+  private boolean playing = true;
+   boolean isWinner = true;
+   
   
-    public BlackjackGame(String name) {
+    public BlackjackGame() {
         super("Blacjack");
     }
 public String displayInfo() {
@@ -28,37 +34,99 @@ private boolean isBust(){
 
     @Override
     public void play() {
-         dealer = new BlackJackDealer();
-         player = new BlackJackPlayer();
+        Scanner input = new Scanner(System.in);
         
+        System.out.println("Welcome to BlackJack \nPlease enter your name:");
+        String name = input.nextLine();
+        System.out.println("Please enter your age:");
+        int age = input.nextInt();
+        
+         try {
+         player = new BlackJackPlayer(age, name);
+        } catch (IllegalArgumentException e) {
+             System.err.println(e.getMessage());
+             System.exit(0);
+        }
+ 
+         dealer = BlackJackDealer.getDealer();
        
         
         
-        Hand dealerHand = dealer.getHand();
-        Hand playerHand = player.getHand();
         
-        CardsDeck deck = dealer.getDeck();
-        
-        playerHand.addCard(deck.sendCard());
-        playerHand.addCard(deck.sendCard());
-        
-        playerHand.addCard(deck.sendCard());
-        
-        //loop to player play() method if not stand or bust
-        player.play();
-        //done with player play loop
-        
-         // start dealer turn;
-        dealer.play();
-        
-        //exit or loop to new round
+        while (playing) {
+            player.getHand().clearHand();
+            dealer.getHand().clearHand();  
+            CardsDeck deck = dealer.getDeck();
+            
+            System.out.printf("%s! You have $%d Plaese place a bet\n", name, player.getCredit());
+            for(int i = 1; i <= bet.length; i++) {
+                System.out.println(i + ": $" + bet[i-1]);
+            }
+            player.setPlayerBet(bet[input.nextInt()-1]);
+            
+            player.getHand().addCard((PlayingCard)deck.sendCard());
+            player.getHand().addCard((PlayingCard)deck.sendCard());
+            
+            if (player.getHand().sum() == 21){
+                declareWinner("player");
+                continue;
+            }
+            
+            dealer.getHand().addCard((PlayingCard)deck.sendCard());
+            
+            System.out.println("In your Hand: \n" + player.getHand() + "\nHand Total is:" + player.getHand().sum());
+            System.out.println("\nIn the Dealer Hand: \n" + dealer.getHand() + "*********");
+            
+            boolean isStand = false;
+            while(!isStand && !player.getHand().isBust()) {
+                System.out.printf("1: %s 2: %s 3: %s\n", Menu.STAND, Menu.HIT, Menu.EXIT);
+               int enter =  input.nextInt();
+                if (enter == 1){
+                    isStand = true;
+                } else if (enter == 1) {
+                    player.getHand().addCard((PlayingCard)deck.sendCard());
+                    System.out.println("In your Hand: \n" + player.getHand() 
+                            + "\nHand Total is: " + player.getHand().sum() 
+                            + "\nYour bet is: $" + player.getPlayerBet());
+                    if (player.getHand().sum() == 21) {
+                        declareWinner("player");
+                    }
+                    if (player.getHand().isBust()) {
+                        declareWinner("dealer");
+                    }
+                } else {
+                    break;
+                }
+            }
+             
+             dealer.play();
+             if (dealer.getHand().sum() > player.getHand().sum() 
+                 && dealer.getHand().sum() <= 21) {
+                 
+                 declareWinner("dealer");
+             } else {
+                declareWinner("player"); 
+             }
+           
+        }
+       
         
         
     }
 
     @Override
-    public void declareWinner() {
-        
+    public void declareWinner(String winner) {
+       
+        if (winner.equals("dealer")) {
+            System.out.println("Dealer Winns!");
+            player.setCredit(player.getPlayerBet(), !isWinner);
+            System.out.printf("%s! You have $%d \n", player.getName(), player.getCredit());
+           
+        } else {
+            System.out.println("You Win!");
+            player.setCredit(player.getPlayerBet(), isWinner);
+            System.out.printf("%s! You have $%d \n", player.getName(), player.getCredit());
+        }
     }
 
 }
